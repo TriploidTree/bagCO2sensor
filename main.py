50464b7c38ce47991d87812416e55e576f7a8b20
Triona <triona@gmail.com>
2023-01-28 17:27:40 +0000

    Updated to be event driven. Removed loop Start and stop functions key to getting it working more reliably.
    Shout out to Fuzzix for doing all the figuring out.

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ import time
 import badger2040
 import pimoroni_i2c
 import breakout_scd41
+from machine import Timer
 
+DEBUG_ENABLE = 0
+MAX_ERROR_RESET = 5
 PINS_BREAKOUT_GARDEN = {"sda": 4, "scl": 5}
 PINS_PICO_EXPLORER = {"sda": 20, "scl": 21}
+
 
 badger = badger2040.Badger2040()
 
 i2c = pimoroni_i2c.PimoroniI2C(**PINS_BREAKOUT_GARDEN)
 
 breakout_scd41.init(i2c)
+breakout_scd41.stop()
 breakout_scd41.start()
 
+updating = False
+error_count = 0
 
 button_a = machine.Pin(badger2040.BUTTON_A, machine.Pin.IN, machine.Pin.PULL_DOWN)
 button_b = machine.Pin(badger2040.BUTTON_B, machine.Pin.IN, machine.Pin.PULL_DOWN)
@@ -21,29 +28,64 @@ button_c = machine.Pin(badger2040.BUTTON_C, machine.Pin.IN, machine.Pin.PULL_DOW
 button_up = machine.Pin(badger2040.BUTTON_UP, machine.Pin.IN, machine.Pin.PULL_DOWN)
 button_down = machine.Pin(badger2040.BUTTON_DOWN, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
-def CO2monitor():
-    if breakout_scd41.ready():
-        co2, temperature, humidity = breakout_scd41.measure()
-        print(co2, temperature, humidity)
-        
-        badger.pen(15)
-        badger.clear()
-        
-        badger.pen(0)
-        badger.thickness(3)
-        badger.text("Air monitor:", 60, 25)
-        badger.thickness(2)
-        badger.text(str(int(co2)) +" ppm CO2" , 20, 70)
-        badger.text (str(int(temperature)) +" ºC    " + str(int(humidity)) + " %RH" , 20, 100)
-        badger.update()
-    else:
-        print("wtf")
+def DEBUG(*msg):
+    if DEBUG_ENABLE:
+        print(msg)
 
-def anything(flibble):
-    print("button pressed")
+def draw(co2, temperature, humidity):
+    badger.pen(15)
+    badger.clear()
+    badger.pen(0)
+    badger.thickness(3)
+    badger.text("Air monitor:", 60, 25)
+    badger.thickness(2)
+    badger.text(str(int(co2)) +" ppm CO2" , 20, 70)
+    badger.text (str(int(temperature)) +" ºC    " + str(int(humidity)) + " %RH" , 20, 100)
+    badger.update()
+    
+
+def CO2monitor():
+    DEBUG("wah!")
+    global updating
+    global error_count
+    if not updating:
+        updating = True
+        DEBUG("UPDATING")
+        co2, temperature, humidity = (0,0,0)
+        if breakout_scd41.ready(): # <--- blocking
+            DEBUG("READY")
+            try:
+                co2, temperature, humidity = breakout_scd41.measure()
+            except Exception as e:
+                DEBUG("Failed to get scd41 data")
+                DEBUG(e)
+                updating = False
+                return
+            DEBUG(co2, temperature, humidity)
+        else:
+            DEBUG("wtf")
+            error_count = error_count + 1
+            if error_count >= MAX_ERROR_RESET:
+                breakout_scd41.stop()
+                breakout_scd41.start()
+
+        draw(co2, temperature, humidity)
+        DEBUG("SLEEPING")
+        time.sleep(10)
+        
+        updating = False
+        DEBUG("DONE")
+
+    else:
+        return
+
+def refresh_cb(flibble):
+    DEBUG("button pressed")
     CO2monitor()
 
-button_a.irq(trigger=machine.Pin.IRQ_RISING, handler=anything)
+button_a.irq(trigger=machine.Pin.IRQ_RISING, handler=refresh_cb)
+timer=Timer(-1)
+timer.init(period=120000, mode=Timer.PERIODIC, callback=lambda t:CO2monitor())
 #button_b.irq(trigger=machine.Pin.IRQ_RISING, handler=button)
 #button_c.irq(trigger=machine.Pin.IRQ_RISING, handler=button)
 
@@ -51,8 +93,5 @@ button_a.irq(trigger=machine.Pin.IRQ_RISING, handler=anything)
 #button_down.irq(trigger=machine.Pin.IRQ_RISING, handler=button)
 #button_user.irq(trigger=machine.Pin.IRQ_RISING, handler=button)
 
-
-
-while True:
-    CO2monitor()
-    time.sleep(120) 
+time.sleep(5)
+CO2monitor()
